c71ce6edfe20be400e3ce82bdd5c1db0eb212a4d
Pedro <pedromendes7182@gmail.com>
2025-11-27 16:31:29 -0300

    feat(sales): implementa a interface IOrderRepository

---
+++ src/SalesService.Api/Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using SalesService.Api.Domain.Entities;
+
+namespace SalesService.Api.Domain.Interfaces;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order);
+    Task<Order?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task UpdateAsync(Order order);
+    Task DeleteAsync(Order order);
+    Task SaveChangesAsync();
+}
---
+++ src/SalesService.Api/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesService.Api.Domain.Entities;
+using SalesService.Api.Domain.Interfaces;
+using SalesService.Api.Infrastructure.Database;
+
+namespace SalesService.Api.Infrastructure.Repositories;
+
+public class OrderRepository(SalesDbContext context) : IOrderRepository
+{
+    public async Task AddAsync(Order order)
+    {
+        await context.Orders.AddAsync(order);
+    }
+
+    public async Task<Order?> GetByIdAsync(Guid id)
+    {
+        return await context.Orders
+            .Include(o => o.Items)
+            .Where(o => !o.IsDeleted && o.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await context.Orders
+            .Include(o => o.Items)
+            .Where(o => !o.IsDeleted)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public Task UpdateAsync(Order order)
+    {
+        context.Orders.Update(order);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Order order)
+    {
+        order.SoftDelete();
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
