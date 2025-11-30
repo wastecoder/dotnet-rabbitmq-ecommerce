1bf5683d4c314d0907ef7de95866140ea439bbc3
Pedro <pedromendes7182@gmail.com>
2025-11-30 05:40:03 -0300

    feat: adiciona RabbitMqSettings.cs

---
+++ src/InventoryService.Api/Infrastructure/Configurations/RabbitMqSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryService.Api.Infrastructure.Configurations;
+
+public class RabbitMqSettings
+{
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ExchangeName { get; set; } = string.Empty;
+    public bool Durable { get; set; }
+    public int RetryCount { get; set; }
+}
