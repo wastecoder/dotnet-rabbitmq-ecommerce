c2a63c19a79a264dd1880f6325cc44ccae9d764b
Pedro <pedromendes7182@gmail.com>
2026-01-23 01:42:59 -0300

    feat(gateway): implementa StockUpdatedConsumer

---
+++ src/Gateway.Api/Infrastructure/Messaging/Consumers/StockUpdatedConsumer.cs
@@ -0,0 +1,17 @@
+﻿using Gateway.Api.Domain.Interfaces;
+using Gateway.Api.Infrastructure.Messaging.Abstractions;
+using Gateway.Api.Presentation.Integration.Events.Inventory;
+
+namespace Gateway.Api.Infrastructure.Messaging.Consumers;
+
+public class StockUpdatedConsumer(
+    IProductMetricsService productMetricsService
+) : IRabbitMqConsumer<StockUpdatedEvent>
+{
+    public async Task HandleAsync(
+        StockUpdatedEvent message,
+        CancellationToken cancellationToken)
+    {
+        await productMetricsService.UpdateStockMetricsAsync(message);
+    }
+}
